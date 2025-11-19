a06f2aa20f747430e1053e968fbf3f35f2ca9956
Road <dantegabrielaguerorodriguez@gmail.com>
2025-11-19 14:55:04 -0300

    Actualizacion de iconos de MiniPOS a POSoffice

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -150,7 +150,7 @@ class _LoginScreenState extends State<LoginScreen> with SingleTickerProviderStat
                             ),
                             const SizedBox(height: 8),
                             Text(
-                              'Sistema de Gestión Empresarial',
+                              'Punto de venta para carga y transporte de pasajeros',
                               style: TextStyle(
                                 fontSize: isDesktop ? 14 : 12,
                                 color: Colors.grey[600],
